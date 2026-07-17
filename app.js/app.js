00448fe4458f3bd6c6e3639220a1f6a55f2086ec
tinithi-perera <tinithiperera@gmail.com>
2026-07-17 11:40:53 +0530

    Refactor code by commenting out unused Student class and initial console log

--- app.js/app.js
+++ app.js/app.js
@@ -1,1 +1,24 @@
-console.log("hello");
+// console.log("hello");
+
+// class Student{
+//     name;
+//     age;
+//     address;
+
+//     constructor(name,age,address){
+//         this.name=name;
+//         this.age=age;
+//         this.address=address;
+//     }
+// }
+ 
+// let s1=new Student("Tinithi",22,"moratuwa");
+// console.log(s1);
+
+
+let c2={
+    name:"Tinithi",
+    age:22,
+    address:"Moratuwa" 
+};
+console.log(c2);
