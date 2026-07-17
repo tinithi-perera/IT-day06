5070ef939e8ed9dee8050e5037f83c12c9c6eee2
tinithi-perera <tinithiperera@gmail.com>
2026-07-17 17:37:35 +0530

    Refactor app.js and index.html to comment out unused functions and add new input fields

--- app.js/app.js
+++ app.js/app.js
@@ -67,25 +67,55 @@
 
 // document.write("<h2>tintin</h2>");
 
-let heading=document.getElementById("heading");
-// // console.log(heading);
+// let heading=document.getElementById("heading");
+// // // console.log(heading);
 
-// heading.innerText="Hiiii tintin";
-let num=0;
-function increase(){
-    console.log("clicked");
+// // heading.innerText="Hiiii tintin";
+// let num=0;
+// function increase(){
+//     console.log("clicked");
     
-    heading.innerText=++num;
+//     heading.innerText=++num;
     
-}
-function decrease(){
-    console.log("clicked");
+// }
+// function decrease(){
+//     console.log("clicked");
     
-    heading.innerText=--num;
+//     heading.innerText=--num;
     
-}
+// }
 
 
+// function changetext(){
+//     let textint=document.getElementById("inputtext");
+//     let heading=document.getElementById("heading");
+//     heading.innerText=textint.value;
+    
+//     console.log("clicked");
+    
+// }
+
+// function addition(){
+//      let textint1=document.getElementById("num1");
+//      let textint2=document.getElementById("num2");
+//      let num1=textint1.value;
+//      let num2=textint2.value;
+//     //  let result=Number(num1) + Number(num2);
+//      let result=parseInt(num1)+parseInt(num2);
+//      let heading=document.getElementById("heading");
+//      heading.innerText=result;
+//      console.log(result);
+// }
 
 
+// function substraction(){
+//      let textint1=document.getElementById("num1");
+//      let textint2=document.getElementById("num2");
+//      let num1=textint1.value;
+//      let num2=textint2.value;
+//      let result=(num1) - (num2);
+//      let heading=document.getElementById("heading");
+//      heading.innerText=result;
+//      console.log(result);
+// }
 
