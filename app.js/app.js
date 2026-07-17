2306e782666db8038bc056d9b3697d7fbd4c70c8
tinithi-perera <tinithiperera@gmail.com>
2026-07-17 11:57:14 +0530

    Add object properties and console logs for 'c2' object

--- app.js/app.js
+++ app.js/app.js
@@ -19,6 +19,21 @@
 let c2={
     name:"Tinithi",
     age:22,
-    address:"Moratuwa" 
+    address:"Moratuwa",
+    salary: 50000,
+    items:[0,1,2,3,4],
+    farther:{
+        name:"padmalal",
+        age:60,
+        address:"moratuwa"
+    }
 };
 console.log(c2);
+console.log(c2.name);
+console.log(c2.age);
+console.log(c2.salary);
+console.log(c2.items);
+console.log(c2.farther.name);
+console.log(c2.items[1]);
+
+
